a165120f6acd40dbb3f7148072821b84ed08673f
Akanksha10029 <akanksharani405@gmail.com>
2024-08-17 03:00:43 +0530

    updated

--- maindjango/review/forms.py
+++ maindjango/review/forms.py
@@ -4,13 +4,33 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['text','photo','rating']
+        fields = ['video_title', 'video_url','text','photo','rating']
         labels = {
+            'video_title': 'Enter video name',
+            'video_url': 'Enter video url',
             'text': 'Your Review',
             'photo': 'Upload an Image',
             'rating': 'Rate the Channel',
         }
         widgets = {
-            'text': forms.Textarea(attrs={'rows': 5, 'placeholder': 'Write your review here...'}),
-            'rating': forms.Select(choices=Review.RATING_CHOICES),
+            'video_title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter the title of the video',
+                'maxlength': '255'
+            }),
+            'video_url': forms.URLInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter the URL of the video'
+            }),
+            'text': forms.Textarea(attrs={
+                'rows': 5,
+                'class': 'form-control',
+                'placeholder': 'Write your review here...'
+            }),
+            'rating': forms.Select(choices=Review.RATING_CHOICES, attrs={
+                'class': 'form-control'
+            }),
+            'photo': forms.ClearableFileInput(attrs={
+                'class': 'form-control-file'
+            }),
         }
