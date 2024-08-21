e46aa4bdc0b046675429ddb41448baca92f5dba9
Akanksha10029 <akanksharani405@gmail.com>
2024-08-21 23:41:16 +0530

    added urls of login and logout

--- maindjango/review/urls.py
+++ maindjango/review/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
+    path('review/', views.review_list, name='review_list'),
     path('', views.review_list, name='review_list'),
     path('create/', views.review_create, name='review_create'),
     path('<int:review_id>/edit/', views.review_edit, name='review_edit'),
     path('<int:review_id>/delete/', views.review_delete, name='review_delete'),
     path('my_reviews/', views.my_reviews, name='my_reviews'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
     path('register/', views.register, name='register'),
     
 ] 
